62925105ead755c7182331674f18a505dcb3e406
rayadu <57172925+rayadu@users.noreply.github.com>
2021-08-24 12:48:52 +0530

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,10 @@
+#learning how to use variables
+rent=int(input(print("please enter the rent for this month")))
+water=int(input(print("please enter the water expense for this month")))
+milk=int(input(print("please enter the milk expense for this month")))
+current_expense=int(input(print("please enter the electricity expense for this month")))
+petrol=int(input(print("please enter the petrol expense for this month")))
+groceries=int(input(print("please enter the groceries expense for this month")))
+mischelenous=int(input(print("please enter the mischekenous expense for this month")))
+total=rent+water+milk+current_expense+petrol+groceries+mischelenous
+print(total)
